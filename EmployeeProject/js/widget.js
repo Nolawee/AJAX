b6b4cbfc68ad70c068655d179229bf72bb0ad331
Nolawee Mengist <nome9478@colorado.edu>
2017-11-16 14:21:53 -0700

    Stage 2 Challenge

--- EmployeeProject/js/widget.js
+++ EmployeeProject/js/widget.js
@@ -16,5 +16,26 @@ xhr.onreadystatechange = function () {
     document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
-xhr.open('GET', 'data/employees.json');
+xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+var roomRequest = new XMLHTTPRequest();
+roomRequest.onreadystatechange = function () {
+  if (roomRequest.readyState === 4) {
+    var rooms = JSON.parse(roomRequest.responseText);
+    var statusHTML = '<ul class=bulleted>';
+    for(var i = 0; i < rooms.length; i+=1){
+      if(rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+roomRequest.open('GET', '../data/rooms.json');
+roomRequest.send();
